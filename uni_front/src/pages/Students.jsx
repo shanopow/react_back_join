bde89d0896265ed644675636a3c580d2a1e60b79
Shane Power <shane@localhost.localdomain>
2023-04-03 14:26:19 +0100

    Added last get for students

---
+++ uni_front/src/pages/Students.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Students() {  
+    return(
+        <div>
+        <p>Use "students/id" for individual pages</p>
+        </div>
+    );
+}
+
+export default Students;
